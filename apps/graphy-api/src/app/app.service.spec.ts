c0ccf5f68a8e8dee2b30ecd3a0239bd004fba841
Austin Stewart <astewart@texasdevs.com>
2023-03-15 09:44:00 -0500

    API Generate

---
+++ apps/graphy-api/src/app/app.service.spec.ts
@@ -0,0 +1,21 @@
+import { Test } from '@nestjs/testing';
+
+import { AppService } from './app.service';
+
+describe('AppService', () => {
+  let service: AppService;
+
+  beforeAll(async () => {
+    const app = await Test.createTestingModule({
+      providers: [AppService],
+    }).compile();
+
+    service = app.get<AppService>(AppService);
+  });
+
+  describe('getData', () => {
+    it('should return "Welcome to graphy-api!"', () => {
+      expect(service.getData()).toEqual({ message: 'Welcome to graphy-api!' });
+    });
+  });
+});
